1d69d1de0cfb2bf7bbbdbc20d30cc1178e2adeb2
Andrew Blackford <62357205+acblackford@users.noreply.github.com>
2023-06-08 22:48:52 -0500

    Update storm.py
    added city plotting into a helper function in wedgj_utils

--- storm.py
+++ storm.py
@@ -129,21 +129,7 @@ class storm:
     
     #Add cities to state plots:
     if domain == 'Ohio':
-      ax.add_feature(USCOUNTIES.with_scale('500k'), lw = 0.1, edgecolor = 'black')        
-      Dayton = gpd.tools.geocode('Dayton, OH')
-      Cincinnati = gpd.tools.geocode('Cincinnati, OH')
-      Columbus = gpd.tools.geocode('Columbus, OH')
-      Cleveland = gpd.tools.geocode('Cleveland, OH')
-      Toledo = gpd.tools.geocode('Toledo, OH')
-      Akron = gpd.tools.geocode('Akron, OH')
-      oh_cities = [Dayton['geometry'][0], Cincinnati['geometry'][0], Columbus['geometry'][0], Cleveland['geometry'][0], Toledo['geometry'][0], Akron['geometry'][0]]
-      oh_x = [point.x for point in oh_cities]
-      oh_y = [point.y for point in oh_cities]
-
-      oh_cities_labels = [Dayton['address'][0], Cincinnati['address'][0]]
-      ax.scatter(oh_x, oh_y, color = 'None', edgecolor = 'black', s = 100, marker = 's', linewidths = 2, transform = ccrs.crs.PlateCarree(), label = oh_cities_labels, zorder = 10)
-      #ax.annotate(oh_cities_labels, (oh_x, oh_y), textcoords="offset points", xytext=(3,3), ha='center')   
-
+        wedgj_utils.plot_oh_cities(self, ax)
 
     shpfilename = shpreader.natural_earth(resolution='110m',
                                         category='cultural',
@@ -325,25 +311,8 @@ class storm:
       
       #Add cities to state plots:
       if domain == 'Ohio':
-        Dayton = gpd.tools.geocode('Dayton, OH')
-        Cincinnati = gpd.tools.geocode('Cincinnati, OH')
-        Columbus = gpd.tools.geocode('Columbus, OH')
-        Cleveland = gpd.tools.geocode('Cleveland, OH')
-        Toledo = gpd.tools.geocode('Toledo, OH')
-        Akron = gpd.tools.geocode('Akron, OH')
-
-        oh_cities = [Dayton['geometry'][0], Cincinnati['geometry'][0], Columbus['geometry'][0], Cleveland['geometry'][0], Toledo['geometry'][0], Akron['geometry'][0]]
-        oh_x = [point.x for point in oh_cities]
-        oh_y = [point.y for point in oh_cities]
-
-        oh_cities_labels = [Dayton['address'][0].split(',')[0], Cincinnati['address'][0].split(',')[0], Columbus['address'][0].split(',')[0], Cleveland['address'][0].split(',')[0], Toledo['address'][0].split(',')[0], Akron['address'][0].split(',')[0]]
-        ax.scatter(oh_x, oh_y, color = 'None', edgecolor = 'black', s = 100, marker = 's', linewidths = 2, transform = ccrs.crs.PlateCarree(), zorder = 10)
-        ax.add_feature(USCOUNTIES.with_scale('500k'), lw = 0.1, edgecolor = 'black') 
-
-        for i in range(len(oh_x)):
-          ax.text(oh_x[i]+0.1, oh_y[i]+0.1, oh_cities_labels[i], horizontalalignment='left', verticalalignment='center', transform = ccrs.crs.PlateCarree(), fontweight = 'bold', font = 'Liberation Serif', fontsize = 12)
-
-          
+        wedgj_utils.plot_oh_cities(self, ax)
+        
       shpfilename = shpreader.natural_earth(resolution='110m',
                                           category='cultural',
                                           name='admin_0_countries')
@@ -821,6 +790,10 @@ class storm:
       #Add counties if not CONUS:
       if domain != 'CONUS':
         ax.add_feature(USCOUNTIES.with_scale('500k'), lw = 0.1, edgecolor = 'black') 
+      
+      #Add cities to state plots:
+      if domain == 'Ohio':
+        wedgj_utils.plot_oh_cities(self, ax)
       
       shpfilename = shpreader.natural_earth(resolution='110m',
                                           category='cultural',
