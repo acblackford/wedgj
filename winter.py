a47b3b342f5772ded5b2e16fa6729d07678b3c70
Andrew Blackford <62357205+acblackford@users.noreply.github.com>
2023-07-13 14:59:24 -0500

    Add files via upload

--- winter.py
+++ winter.py
@@ -139,4 +139,145 @@ class winter:
         print(err)
         
   def hrrr_snow(self, spath = None, domain = 'CONUS', type = 'kuchera'):
-    exit
+ 
+    #Set up initial file and series of files:
+    # Location of HRRR data:
+    hrrr_url = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/hrrr/prod/"
+
+    # Build the HRRR urls
+    urls = []
+    date = datetime.utcnow()
+    if (date.hour >= 21):
+        date = date.replace(hour = 18)
+    elif (date.hour >= 15):
+        date = date.replace(hour = 12)
+    elif (date.hour >= 9):
+        date = date.replace(hour = 6)
+    elif (date.hour >= 3):
+        date = date.replace(hour = 0)
+    else:
+        date = date.replace(hour = 18)-timedelta(days=1)
+
+    for i in range(49):
+        urls.append(hrrr_url+"hrrr.{}/conus/hrrr.t{:02d}z.wrfprsf{:02d}.grib2".format(
+            date.strftime("%Y%m%d"), date.hour, i))
+            
+    ### Loop over the run, downloading just the necessary fields:
+    try:
+        for i, url in enumerate(urls):
+            # Download index file
+            os.system("curl -o hrrr_index {}".format(url+".idx"))
+
+            # Open index file to find relevant bytes
+            f = open("hrrr_index", "r")
+            lines = list(f)
+            f.close()
+
+            ### Do each variable in turn:
+            data = {}
+            for var in [("ASNOW", "surface"),  #in units of m
+                         ("TMP", "2 m above ground"),
+                         ("TMP", "850 mb"),
+                         ("TMP", "700 mb"),
+                         ("TMP", "500 mb")]: 
+
+                # Check the index
+                flag = False
+                for line in lines:
+                    # Check if var already found
+                    if flag:
+                        byte_end = line.split(":")[1]
+                        break
+
+                    # Locate variable
+                    if ((var[0] in line) and (var[1] in line)):
+                        byte_start = line.split(":")[1]
+                        flag = True
+
+                # Download the HRRR data
+                os.system("curl -o hrrr_out -r {}-{} {}".format(byte_start, byte_end, url))
+
+                # Grab the data
+                grib = pygrib.open("hrrr_out")
+                data[var[0]+var[1]] = grib[1].values
+                lons = grib[1].longitudes.reshape(data[var[0]+var[1]].shape)
+                lats = grib[1].latitudes.reshape(data[var[0]+var[1]].shape)
+                vd = grib[1].validDate
+                ad = grib[1].analDate
+                grib.close()
+
+            #Calculate 10:1 Ratio Snowfall Accumulation:
+            snow = data["ASNOW"+"surface"] * 10 * 0.04 /0.01
+                        
+            #Calculate Kuchera Method Snowfall Accumulation:
+            temp = np.array([data["TMP"+"2 m above ground"],data["TMP"+"850 mb"],data["TMP"+"700 mb"],data["TMP"+"500 mb"]])
+            
+            kuchera = np.zeros(lons.shape)
+
+            tmax = np.nanmax(temp, axis = 0)
+            
+            for t in range(temp.shape[0]):
+                print(temp.mean(), temp.max(), temp.min(), t)
+            
+            for k in range(kuchera.shape[1]):
+                for j in range(kuchera.shape[0]):
+                    if tmax[j,k] > 271.16:
+                        kuchera[j,k] = 12 + 2*(271.16 - tmax[j,k])
+                    else:
+                        kuchera[j,k] = 12 + (271.16 - tmax[j,k])
+
+            print(np.nanmean(kuchera), np.nanmax(kuchera), np.nanmin(kuchera))
+            
+            kuchera = kuchera * ((snow)/10)
+            print(np.nanmean(snow/10.0), np.nanmax(snow/10.0), np.nanmin(snow/10.0))
+            print(np.nanmean(kuchera), np.nanmax(kuchera), np.nanmin(kuchera))
+            
+            kuchera[kuchera < 0.01] = np.nan        
+            snow[snow < 0.01] = np.nan
+
+            #Set dictionary to look up user input snow type for plotting:
+            snow_type = {'kuchera':kuchera, '10:1': snow, 'model': model_snow}
+            snow_type_title = {'kuchera': 'Kuchera Method', '10:1': '10:1', 'model': 'Model Ratio'}
+            
+            #Make figure:
+            fig, ax = plt.subplots(figsize = figsize_table[domain], subplot_kw = {'projection' : ccrs.crs.PlateCarree()})
+
+            extent_table = wedgj_utils.extent_table(self)
+
+            #Add cartopy boundaries::
+            try:
+              ax.set_extent(extent_table[domain])
+            except:
+              ax.set_extent(extent_table['CONUS'])
+              print('Invalid Domain Input. Setting extent to CONUS.')
+            
+            snow_table = wedgj_utils.snow_cmap(self)
+            
+            barb_int = 16
+            snowcf = ax.contourf(lons, lats, snow, cmap=snow_table['snow_cmap'], levels = snow_table['snow_clevs'], norm=mcolors.BoundaryNorm(snow_table['snow_clevs'], snow_table['snow_cmap'].N), transform = ccrs.crs.PlateCarree(), extend = 'max')
+
+            #Add counties if not CONUS:
+            if domain != 'CONUS':
+              wedgj_utils.plot_counties(self, ax)
+            
+            #Add cities to state plots:
+            if domain == 'Ohio':
+                wedgj_utils.plot_oh_cities(self, ax)
+            elif domain == 'Indiana':
+                wedgj_utils.plot_in_cities(self, ax)
+            elif domain == 'Alabama':
+                wedgj_utils.plot_al_cities(self, ax)
+            else:
+                pass
+
+            #Add cartopy boundaries:
+            wedgj_utils.add_geog_ref(self, ax)
+            
+            cbar = plt.colorbar(snowcf, orientation = 'horizontal', label = 'Accumulated Snowfall (in)', extendrect = True, aspect = 65, pad = 0)
+            cbar.set_ticks((0, 1, 3, 5, 8, 12, 18, 24, 36, 48, 60))
+            plt.title(f'Model Ratio Accumulated Snowfall (in)\{domain} Region', loc = 'left', fontweight = 'bold', fontsize = 10)
+            plt.title('Valid: {}\n{} HRRR F{:03d}'.format(ad+timedelta(hours=i),ad.strftime("%Y%m%d %HZ"),i), loc = 'right', fontsize = 10)
+            plt.savefig('{}f{:03d}.png'.format(spath, i))
+            
+    except Exception as err:
+        print(err)
